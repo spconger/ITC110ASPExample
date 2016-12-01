cc1759d8f8138c7b8a0f515e7cbe7446e34e05c1
Steve Conger <steve@spconger.com>
2016-12-01 12:24:21 -0800

    Commit MealOrder class

---
+++ App_Code/MealOrder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MealOrder
+/// </summary>
+public class MealOrder
+{
+    /// <summary>
+    /// This class bundles the information about
+    /// the meal. Specifically it keeps a list
+    /// of the entrees and ahd drinks
+    /// it has methods for adding items to these
+    /// two lists.
+    /// It also has a fairly unrealistic calculate
+    /// method to figure out the total
+    /// </summary>
+    /// 
+    //create public properties of the lists
+    public List<string> Entrees { set; get; }
+    public List<string> Drinks { set; get; }
+    //initialize lists in the constructor
+    public MealOrder()
+    {
+        Entrees = new List<string>();
+        Drinks = new List<string>();
+    }
+
+    //add entrees to list
+    public void AddEntree(string item)
+    {
+        Entrees.Add(item);
+    }
+
+    //add drinks
+    public void AddDrink(string item)
+    {
+        Drinks.Add(item);
+    }
+
+    public double CalculateTotal()
+    {
+        double total = 0;
+        foreach(string entree in Entrees)
+        {
+            total += 2.99;
+        }
+        foreach(string drink in Drinks)
+        {
+            total += .99;
+        }
+        return total;
+    }
+}
